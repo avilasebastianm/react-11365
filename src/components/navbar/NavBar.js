283272f75ecb873c982700503ede7c6a66d5fb15
Seba <avila.sebastianm@gmail.com>
2021-01-05 10:28:09 -0300

    first commit

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <div className="containerNav">
+      <div className="logo">LOGO</div>
+      <p>HOME</p>
+      <p>Categorias</p>
+      <p>Carrito</p>
+    </div>
+  );
+};
