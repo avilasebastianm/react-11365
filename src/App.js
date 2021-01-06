4d988a2b5a52ef9d8f5406a8b1856c354398993b
sebastian <avila.sebastianm@gmail.com>
2021-01-06 13:40:05 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { NavBar } from "./components/navbar/NavBar";
-import "./components/navbar/navbar.css";
+/*import "./components/navbar/navbar.css";*/
 function App() {
   return (
     <div className="App">
