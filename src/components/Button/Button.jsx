886f61588821a180bf9afea3f27f160518c869cb
Seba <avila.sebastianm@gmail.com>
2021-01-07 09:11:13 -0300

    Seba

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Button.css";
+
+export const Button = () => {
+  return (
+    <div className="button">
+      <button>Click</button>
+    </div>
+  );
+};
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+import { Button } from "../Button/Button";
+
+export const Home = () => {
+  return (
+    <div className="homeStyle">
+      <Button />
+    </div>
+  );
+};
